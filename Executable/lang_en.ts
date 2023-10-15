3ee1a4202d40522fc8322e8a579e2d04b929533c
Maksymr0906 <maksymr0906@gmail.com>
2023-10-15 19:08:26 +0300

    add .exe

---
+++ Executable/lang_en.ts
@@ -0,0 +1,358 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>TaskDialog</name>
+    <message>
+        <source>Dialog</source>
+        <translation type="vanished">Dialog</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="14"/>
+        <source>Task Menu</source>
+        <translation>Task Menu</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="46"/>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="88"/>
+        <source>Responsible</source>
+        <translation>Responsible</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="118"/>
+        <source>Task Name</source>
+        <translation>Task Name</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="130"/>
+        <source>Deadline</source>
+        <translation>Deadline</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="154"/>
+        <source>Add to Important view</source>
+        <translation>Add to Important view</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="166"/>
+        <source>Add to My Day view</source>
+        <translation>Add to My Day view</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="184"/>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="204"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.cpp" line="25"/>
+        <source>Invalid deadline</source>
+        <translation>Invalid deadline</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.cpp" line="30"/>
+        <source>Valid deadline</source>
+        <translation>Valid deadline</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.cpp" line="36"/>
+        <source>Invalid email</source>
+        <translation>Invalid email</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.cpp" line="41"/>
+        <source>Valid email</source>
+        <translation>Valid email</translation>
+    </message>
+</context>
+<context>
+    <name>TasksTableView</name>
+    <message>
+        <location filename="TasksTableView.cpp" line="37"/>
+        <source>Task</source>
+        <translation>Task</translation>
+    </message>
+    <message>
+        <location filename="TasksTableView.cpp" line="38"/>
+        <source>Deadline</source>
+        <translation>Deadline</translation>
+    </message>
+    <message>
+        <location filename="TasksTableView.cpp" line="39"/>
+        <source>Responsible</source>
+        <translation>Responsible</translation>
+    </message>
+    <message>
+        <location filename="TasksTableView.cpp" line="40"/>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+</context>
+<context>
+    <name>ToDoList</name>
+    <message>
+        <location filename="ToDoList.cpp" line="16"/>
+        <location filename="ToDoList.cpp" line="291"/>
+        <location filename="ToDoList.cpp" line="310"/>
+        <location filename="ToDoList.cpp" line="426"/>
+        <location filename="ToDoList.cpp" line="441"/>
+        <location filename="ToDoList.cpp" line="473"/>
+        <location filename="ToDoList.cpp" line="488"/>
+        <location filename="ToDoList.cpp" line="502"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="426"/>
+        <source>Adding task error: %1</source>
+        <translation>Adding task error: %1</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="441"/>
+        <location filename="ToDoList.cpp" line="488"/>
+        <source>Choose the row at first</source>
+        <translation>Choose the row at first</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="310"/>
+        <location filename="ToDoList.cpp" line="473"/>
+        <source>Updating task error: %1</source>
+        <translation>Updating task error: %1</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="502"/>
+        <source>Deleting task error: %1</source>
+        <translation>Deleting task error: %1</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="94"/>
+        <location filename="ToDoList.cpp" line="138"/>
+        <location filename="ToDoList.cpp" line="336"/>
+        <location filename="ToDoList.cpp" line="361"/>
+        <source>My Day</source>
+        <translation>My Day</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="16"/>
+        <source>Error opening database</source>
+        <translation>Error opening database</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="95"/>
+        <location filename="ToDoList.cpp" line="337"/>
+        <location filename="ToDoList.cpp" line="364"/>
+        <source>Important</source>
+        <translation>Important</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="96"/>
+        <location filename="ToDoList.cpp" line="338"/>
+        <location filename="ToDoList.cpp" line="367"/>
+        <source>All</source>
+        <translation>All</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="97"/>
+        <location filename="ToDoList.cpp" line="339"/>
+        <location filename="ToDoList.cpp" line="370"/>
+        <source>Planned</source>
+        <translation>Planned</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="98"/>
+        <location filename="ToDoList.cpp" line="340"/>
+        <location filename="ToDoList.cpp" line="373"/>
+        <source>Completed</source>
+        <translation>Completed</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="99"/>
+        <location filename="ToDoList.cpp" line="341"/>
+        <location filename="ToDoList.cpp" line="376"/>
+        <source>Failed</source>
+        <translation>Failed</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="108"/>
+        <location filename="ToDoList.cpp" line="342"/>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="109"/>
+        <location filename="ToDoList.cpp" line="343"/>
+        <source>About program</source>
+        <translation>About program</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="112"/>
+        <location filename="ToDoList.cpp" line="344"/>
+        <source>Language</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="113"/>
+        <location filename="ToDoList.cpp" line="345"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="114"/>
+        <location filename="ToDoList.cpp" line="346"/>
+        <source>Ukrainian</source>
+        <translation>Ukrainian</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="119"/>
+        <location filename="ToDoList.cpp" line="347"/>
+        <source>Task</source>
+        <translation>Task</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="120"/>
+        <location filename="ToDoList.cpp" line="348"/>
+        <source>Mark as Completed</source>
+        <translation>Mark as Completed</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="121"/>
+        <location filename="ToDoList.cpp" line="349"/>
+        <source>Mark as Failed</source>
+        <translation>Mark as Failed</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="219"/>
+        <location filename="ToDoList.cpp" line="245"/>
+        <location filename="ToDoList.cpp" line="258"/>
+        <source>Error making a query</source>
+        <translation>Error making a query</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="245"/>
+        <source>Updating status task error: %1</source>
+        <translation>Updating status task error: %1</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="258"/>
+        <source>Updating is my day error: %1</source>
+        <translation>Updating is my day error: %1</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="515"/>
+        <source>Choose the row to mark as completed</source>
+        <translation>Choose the row to mark as completed</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="524"/>
+        <source>Choose the row to mark as failed</source>
+        <translation>Choose the row to mark as failed</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="562"/>
+        <source>About Program</source>
+        <translation>About Program</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="562"/>
+        <source>Simple ToDoList using QT and PostgreSQL</source>
+        <translation>Simple ToDOList using QT and PostrgreSQL</translation>
+    </message>
+</context>
+<context>
+    <name>ToDoListClass</name>
+    <message>
+        <location filename="ToDoList.ui" line="14"/>
+        <source>ToDoList</source>
+        <translation>ToDoList</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="70"/>
+        <location filename="ToDoList.ui" line="270"/>
+        <source>My Day</source>
+        <translation>My Day</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="225"/>
+        <source>Task</source>
+        <translation>Task</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="232"/>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="236"/>
+        <source>Language</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="249"/>
+        <source>toolBar</source>
+        <translation>toolBar</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="279"/>
+        <source>Important</source>
+        <translation>Important</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="288"/>
+        <source>All</source>
+        <translation>All</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="297"/>
+        <source>Planned</source>
+        <translation>Planned</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="306"/>
+        <source>Completed</source>
+        <translation>Completed</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="315"/>
+        <source>Failed</source>
+        <translation>Failed</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="320"/>
+        <source>Add</source>
+        <translation>Add</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="325"/>
+        <source>Edit</source>
+        <translation>Edit</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="330"/>
+        <source>Remove</source>
+        <translation>Remove</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="335"/>
+        <source>About Program</source>
+        <translation>About Program</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="340"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="345"/>
+        <source>Ukrainian</source>
+        <translation>Ukrainian</translation>
+    </message>
+</context>
+</TS>
---
+++ Executable/lang_uk_UA.ts
@@ -0,0 +1,358 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA">
+<context>
+    <name>TaskDialog</name>
+    <message>
+        <source>Dialog</source>
+        <translation type="vanished">Діалог</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="14"/>
+        <source>Task Menu</source>
+        <translation>Меню Завдання</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="46"/>
+        <source>Email</source>
+        <translation>Електронна Пошта</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="88"/>
+        <source>Responsible</source>
+        <translation>Відповідальний</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="118"/>
+        <source>Task Name</source>
+        <translation>Назва завдання</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="130"/>
+        <source>Deadline</source>
+        <translation>Термін</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="154"/>
+        <source>Add to Important view</source>
+        <translation>Додати до Важливих</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="166"/>
+        <source>Add to My Day view</source>
+        <translation>Додати до Мій День</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="184"/>
+        <source>Cancel</source>
+        <translation>Відміна</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.ui" line="204"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.cpp" line="25"/>
+        <source>Invalid deadline</source>
+        <translation>Неправильний термін</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.cpp" line="30"/>
+        <source>Valid deadline</source>
+        <translation>Правильний термін</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.cpp" line="36"/>
+        <source>Invalid email</source>
+        <translation>Неправильна пошта</translation>
+    </message>
+    <message>
+        <location filename="TaskDialog.cpp" line="41"/>
+        <source>Valid email</source>
+        <translation>Правильна пошта</translation>
+    </message>
+</context>
+<context>
+    <name>TasksTableView</name>
+    <message>
+        <location filename="TasksTableView.cpp" line="37"/>
+        <source>Task</source>
+        <translation>Завдання</translation>
+    </message>
+    <message>
+        <location filename="TasksTableView.cpp" line="38"/>
+        <source>Deadline</source>
+        <translation>Термін</translation>
+    </message>
+    <message>
+        <location filename="TasksTableView.cpp" line="39"/>
+        <source>Responsible</source>
+        <translation>Відповідальний</translation>
+    </message>
+    <message>
+        <location filename="TasksTableView.cpp" line="40"/>
+        <source>Email</source>
+        <translation>Ел. пошта</translation>
+    </message>
+</context>
+<context>
+    <name>ToDoList</name>
+    <message>
+        <location filename="ToDoList.cpp" line="16"/>
+        <location filename="ToDoList.cpp" line="291"/>
+        <location filename="ToDoList.cpp" line="310"/>
+        <location filename="ToDoList.cpp" line="426"/>
+        <location filename="ToDoList.cpp" line="441"/>
+        <location filename="ToDoList.cpp" line="473"/>
+        <location filename="ToDoList.cpp" line="488"/>
+        <location filename="ToDoList.cpp" line="502"/>
+        <source>Error</source>
+        <translation>Помилка</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="426"/>
+        <source>Adding task error: %1</source>
+        <translation>Помилка додання завдання: %1</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="441"/>
+        <location filename="ToDoList.cpp" line="488"/>
+        <source>Choose the row at first</source>
+        <translation>Спершу виберіть рядок</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="310"/>
+        <location filename="ToDoList.cpp" line="473"/>
+        <source>Updating task error: %1</source>
+        <translation>Помилка оновлення завдання: %1</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="502"/>
+        <source>Deleting task error: %1</source>
+        <translation>Помилка видалення завдання: %1</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="94"/>
+        <location filename="ToDoList.cpp" line="138"/>
+        <location filename="ToDoList.cpp" line="336"/>
+        <location filename="ToDoList.cpp" line="361"/>
+        <source>My Day</source>
+        <translation>Мій День</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="16"/>
+        <source>Error opening database</source>
+        <translation>Помилка відкриття бази даних</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="95"/>
+        <location filename="ToDoList.cpp" line="337"/>
+        <location filename="ToDoList.cpp" line="364"/>
+        <source>Important</source>
+        <translation>Важливі завдання</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="96"/>
+        <location filename="ToDoList.cpp" line="338"/>
+        <location filename="ToDoList.cpp" line="367"/>
+        <source>All</source>
+        <translation>Всі завдання</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="97"/>
+        <location filename="ToDoList.cpp" line="339"/>
+        <location filename="ToDoList.cpp" line="370"/>
+        <source>Planned</source>
+        <translation>Заплановані завдання</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="98"/>
+        <location filename="ToDoList.cpp" line="340"/>
+        <location filename="ToDoList.cpp" line="373"/>
+        <source>Completed</source>
+        <translation>Виконані завдання</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="99"/>
+        <location filename="ToDoList.cpp" line="341"/>
+        <location filename="ToDoList.cpp" line="376"/>
+        <source>Failed</source>
+        <translation>Провалені завдання</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="108"/>
+        <location filename="ToDoList.cpp" line="342"/>
+        <source>Settings</source>
+        <translation>Налаштування</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="109"/>
+        <location filename="ToDoList.cpp" line="343"/>
+        <source>About program</source>
+        <translation>Про програму</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="112"/>
+        <location filename="ToDoList.cpp" line="344"/>
+        <source>Language</source>
+        <translation>Змінити мову</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="113"/>
+        <location filename="ToDoList.cpp" line="345"/>
+        <source>English</source>
+        <translation>Англійська</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="114"/>
+        <location filename="ToDoList.cpp" line="346"/>
+        <source>Ukrainian</source>
+        <translation>Українська</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="119"/>
+        <location filename="ToDoList.cpp" line="347"/>
+        <source>Task</source>
+        <translation>Завдання</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="120"/>
+        <location filename="ToDoList.cpp" line="348"/>
+        <source>Mark as Completed</source>
+        <translation>Позначити як Виконане</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="121"/>
+        <location filename="ToDoList.cpp" line="349"/>
+        <source>Mark as Failed</source>
+        <translation>Позначити як Провалене</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="219"/>
+        <location filename="ToDoList.cpp" line="245"/>
+        <location filename="ToDoList.cpp" line="258"/>
+        <source>Error making a query</source>
+        <translation>Помилка виконання запиту</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="245"/>
+        <source>Updating status task error: %1</source>
+        <translation>Помилка оновлення статусу завдання: %1</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="258"/>
+        <source>Updating is my day error: %1</source>
+        <translation>Помилка оновлення статусу Мій День: %1</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="515"/>
+        <source>Choose the row to mark as completed</source>
+        <translation>Виберіть завдання, щоб позначити як Виконане</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="524"/>
+        <source>Choose the row to mark as failed</source>
+        <translation>Виберіть завдання, щоб позначити як Провалене</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="562"/>
+        <source>About Program</source>
+        <translation>Про програму</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.cpp" line="562"/>
+        <source>Simple ToDoList using QT and PostgreSQL</source>
+        <translation>Простий список завдань використовуючи QT і PostgreSQL</translation>
+    </message>
+</context>
+<context>
+    <name>ToDoListClass</name>
+    <message>
+        <location filename="ToDoList.ui" line="14"/>
+        <source>ToDoList</source>
+        <translation>Список Завдань</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="70"/>
+        <location filename="ToDoList.ui" line="270"/>
+        <source>My Day</source>
+        <translation>Мій День</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="225"/>
+        <source>Task</source>
+        <translation>Завдання</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="232"/>
+        <source>Settings</source>
+        <translation>Налаштування</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="236"/>
+        <source>Language</source>
+        <translation>Змінити мову</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="249"/>
+        <source>toolBar</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="279"/>
+        <source>Important</source>
+        <translation>Важливі</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="288"/>
+        <source>All</source>
+        <translation>Всі</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="297"/>
+        <source>Planned</source>
+        <translation>Заплановані</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="306"/>
+        <source>Completed</source>
+        <translation>Виконані</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="315"/>
+        <source>Failed</source>
+        <translation>Провалені</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="320"/>
+        <source>Add</source>
+        <translation>Додати</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="325"/>
+        <source>Edit</source>
+        <translation>Редагувати</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="330"/>
+        <source>Remove</source>
+        <translation>Видалити</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="335"/>
+        <source>About Program</source>
+        <translation>Про програму</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="340"/>
+        <source>English</source>
+        <translation>Англійська</translation>
+    </message>
+    <message>
+        <location filename="ToDoList.ui" line="345"/>
+        <source>Ukrainian</source>
+        <translation>Українська</translation>
+    </message>
+</context>
+</TS>
